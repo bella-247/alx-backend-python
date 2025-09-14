b7dc2520ffd42e7b8c0a5d4d0fb6ecf6f57823a8
abelmekonen <abelmekonen247@gmail.com>
2025-09-14 19:22:22 +0300

    done all the code

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+        
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn.cursor()
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.conn:
+            self.conn.commit()
+            self.conn.close()
+            
+
+if __name__ == "__main__":
+    with DatabaseConnection("users.db") as cursor:
+        cursor.execute("SELECT * FROM users")
+        results = cursor.fetchall()
+        print(results)
---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+class ExecuteQuery:
+    def __init__(self, db_name, query, params = None):
+        self.db_name = db_name
+        self.query = query
+        self.params = params or []
+        self.conn = None
+        self.cursor = None
+        
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        
+        if self.params:
+            self.cursor.execute(self.query)
+        else:
+            self.cursor.execute(self.query)
+        return self.cursor.fetchall()
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.conn:
+            self.conn.commit()
+            self.conn.close()
+            
+if __name__ == "__main__":
+    query = "SELECT * FROM users WHERE age > ?"
+    with ExecuteQuery("users.db", query, (25,)) as results:
+        print(results)
