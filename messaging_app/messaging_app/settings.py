3fb4397814e2952a817ef9851a06e657df8fdb7c
abelmekonen <abelmekonen247@gmail.com>
2025-09-19 17:53:28 +0300

    Add REST framework authentication settings to enforce user authentication

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -87,6 +87,16 @@ DATABASES = {
 }
 
 AUTH_USER_MODEL = 'chats.User'
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated"
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication"
+    ]
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
